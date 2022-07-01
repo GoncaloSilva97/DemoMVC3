765e5eed84c80b5c79a0ba278f1564a8918823bd
Gonçalo Ferreira Silva <goncalo.silva.22679@formandos.cinel.pt>
2022-07-01 13:11:39 +0100

    Create about page

--- DemoMVC3/DemoMVC3.Web/Controllers/HomeController.cs
+++ DemoMVC3/DemoMVC3.Web/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace DemoMVC3.Web.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
